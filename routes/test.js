e518b5bc2ac8f455caeb4f535c72d72edfd4b235
Tuan <tuananhvd1998@gmail.com>
2021-08-17 15:30:02 +0700

    update 2

--- routes/test.js
+++ routes/test.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const testController = require('../controller/TestController');
 
 router.get('/returnUrl/:id',testController.returnUrl);
-router.post('/notifyUrl/:id',testController.notifyUrl);
+//router.post('/notifyUrl/:id',testController.notifyUrl);
 router.post('/MomoApi',testController.testMomoApi);
 router.get('/Momo',testController.testMomo);
 router.post('/testStatus',testController.testStatus);
