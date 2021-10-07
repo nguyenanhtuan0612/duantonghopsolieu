7c73eb13bf8956e6c12ebe8335bcacceb36f26ca
Tuan <tuananhvd1998@gmail.com>
2021-10-07 10:11:27 +0700

    update2

--- controller/TestController.js
+++ controller/TestController.js
@@ -1,6 +1,7 @@
 var request = require('sync-request');
 var Momo = require('../config/momo');
-var crypto = require('crypto')
+var crypto = require('crypto');
+const momo = require('../config/momo');
 var token = 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJPZ21xQ0JMUVhJcjBrWlIwMHdwQmZTZHViRFZIaEFyMEw4RFVUQTlpdE9rIn0.eyJqdGkiOiI5YTBhYzdkYy1jNzEzLTQ5YzYtYjliNS02NTMzYTJiM2NhMGUiLCJleHAiOjE2MjMxMzc5NzYsIm5iZiI6MCwiaWF0IjoxNjIzMTM3Njc2LCJpc3MiOiJodHRwOi8vMTkyLjE2OC4zLjEyOjMxODkzL2F1dGgvcmVhbG1zL01DQk9PSyIsImF1ZCI6IndlYnNpdGUiLCJzdWIiOiIyNWNjYTdjYi02ODM3LTRiZjktOTQ1NC1lZmE1MmNiNzg0NjUiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJ3ZWJzaXRlIiwibm9uY2UiOiJmZDJlMDEzOS01OWEwLTQzMzgtYjU4Yi05MTg1ZGE1YjE5NGQiLCJhdXRoX3RpbWUiOjE2MjMxMzc2NzQsInNlc3Npb25fc3RhdGUiOiJjYjZlMTcxNS00ZmMwLTQzZjAtYjA5Mi00MjMwMjhkNDc4NmYiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHA6Ly9sb2NhbGhvc3Q6MzAwMSJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsidXNlcl9kZWxldGUiLCJkaXNjb3VudF9jcmVhdGUiLCJmaWxlX3ZpZXciLCJmaWxlX2RlbGV0ZSIsInVzZXJfdXBkYXRlIiwicGFydGl0aW9uX2NyZWF0ZSIsInBvc3RfdmlldyIsImV4YW1fZGVsZXRlIiwiZmlsZV91cGRhdGUiLCJnaWZ0UHJvbW90aW9uX2NyZWF0ZSIsImdpZnRTdG9yZV91cGRhdGUiLCJib29rX3ZpZXciLCJleGFtX3VwZGF0ZSIsImFjY2Vzc19jb3Vyc2UiLCJ0eXBlUXVlc3Rpb25zX3ZpZXciLCJjb3Vwb25fdmlldyIsImJvb2tfY3JlYXRlIiwicXVlc3Rpb25fdXBkYXRlIiwiY29tYm9Db3Vyc2VfdXBkYXRlIiwiZ2lmdFBhY2thZ2VfdmlldyIsInVtYV9hdXRob3JpemF0aW9uIiwiY291cnNlX3VwZGF0ZSIsInJldmVudWVfdmlldyIsInRlYWNoZXJfYWN0aW9uIiwidXNlcl92aWV3IiwiY2F0ZWdvcnlfdXBkYXRlIiwicG9zdF9kZWxldGUiLCJjb21taXNzaW9uX3ZpZXciLCJjb3Vwb25fY3JlYXRlIiwiYWNjZXNzX2dlbmVyYWwiLCJ0ZWFjaGVyX3ZpZXciLCJjYXRlZ29yeV9jcmVhdGUiLCJjb21ib0NvdXJzZV9kZWxldGUiLCJyb2xlX2RlbGV0ZSIsInBhcnRpdGlvbl91cGRhdGUiLCJjb3Vwb25fdXBkYXRlIiwibm90aWZpY2F0aW9uX3ZpZXciLCJnaWZ0UHJvbW90aW9uX3ZpZXciLCJwYXJ0aXRpb25fdmlldyIsInN0b3JlX2NvdXBvbl92aWV3IiwicGFnZV9jcmVhdGUiLCJnaWZ0U3RvcmVfdmlldyIsInRyYW5zYWN0aW9uX3ZpZXciLCJib29rX3VwZGF0ZSIsImRpc2NvdW50X2FjdGl2ZSIsImNvdXJzZV92aWV3IiwiYm9va19kZWxldGUiLCJyb2xlX3ZpZXciLCJwYWdlX3VwZGF0ZSIsInF1ZXN0aW9uX3ZpZXciLCJnaWZ0U3RvcmVfY3JlYXRlIiwiZ2lmdFBhY2thZ2VfY3JlYXRlIiwicXVlc3Rpb25fY3JlYXRlIiwicGFnZV9kZWxldGUiLCJnaWZ0UHJvbW90aW9uX3VwZGF0ZSIsImRpc2NvdW50X3VwZGF0ZSIsInBvc3RfdXBkYXRlIiwiZmlsZV9jcmVhdGUiLCJleGFtX2NyZWF0ZSIsImNvdXBvbl9hY3RpdmUiLCJyb2xlX2NyZWF0ZSIsIm9yZGVyX3ZpZXciLCJnaWZ0Y29kZV9zdG9yZV92aWV3Iiwib3JkZXJfYWN0aXZlIiwicm9sZV91cGRhdGUiLCJhY2Nlc3NfYWRtaW4iLCJ1c2VyX2NyZWF0ZSIsImNhdGVnb3J5X3ZpZXciLCJjb3Vyc2VfY3JlYXRlIiwibm90aWZpY2F0aW9uX2NyZWF0ZSIsImRpc2NvdW50X3ZpZXciLCJnaWZ0UGFja2FnZV91cGRhdGUiLCJwb3N0X2NyZWF0ZSIsImNvbWJvQ291cnNlX2NyZWF0ZSIsImNvbWJvQ291cnNlX3ZpZXciLCJwYWdlX3ZpZXciLCJleGFtX3ZpZXciLCJjYXRlZ29yeV9kZWxldGUiLCJnaWZ0c19jb2RlX3ZpZXciXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJuYW1lIjoiTmd1eWVuIEFuaCBUdWFuIiwicHJlZmVycmVkX3VzZXJuYW1lIjoidHVhbmFuaHZkMTk5OEBnbWFpbC5jb20iLCJnaXZlbl9uYW1lIjoiTmd1eWVuIEFuaCBUdWFuIiwiZW1haWwiOiJ0dWFuYW5odmQxOTk4QGdtYWlsLmNvbSJ9.Lkvk6vyfPm5q60A90hR9iYVnpwd-G0t9-r0lKTrB8Np4EGUsIF6YY2ar5Eva2sMdDi5r6BSi2as2aawf7yABS9PmHxMZg0GtvMvQQNjFQZFBmEqxS8Q9KN1EX20z1Vk_xq8htxM1w5neEfHa80vzEZX1GtQTVZ98H2ymH7skkxvP_A4whfjCWEohh5CCeedGJS6duYHA6I2aCOLlxlkUXPHWlO431b5upPmXcoNswrS7ypnbsIBhlhFAYBqfwRM3p7BzJXhkjyEoHGmkN_oHVzGoEfDyiuzcsdhu01QwuafCjJF6eqlpxE2EbSfBRLNAcmJpkL2AmVo7eRDyvnRvTw'
 
 class TestController {
@@ -57,13 +58,13 @@ class TestController {
             var amountStr = "100000";
             var orderId = req.body.id.toString() + '_' +new Date().getTime();
             var orderInfo = "INfoo123"; //Thanh toan don hang
-            var returnUrl = "https://mcbooks.vn/"; //chi viet
+            var redirectUrl = "https://mcbooks.vn/"; //chi viet
             var ipnUrl = baseUrl + "orders/api/v1/orders/momo-notify-url/8351"; //tra ve cho sv 
             var extraData = "extraData";
 
             const rawSignature= "accessKey="+Momo.accessKey+"&amount="+amountStr+
             "&extraData="+extraData+"&ipnUrl="+ipnUrl+"&orderId="+
-            orderId+"&orderInfo=INfoo123&partnerCode=MOMOYN8720210729&redirectUrl=https://mcbooks.vn/&requestId="+requestId+"&requestType=captureWallet"
+            orderId+"&orderInfo="+orderInfo+"&partnerCode="+Momo.partnerCode+"&redirectUrl="+redirectUrl+"&requestId="+requestId+"&requestType=captureWallet"
             console.log(rawSignature)
             const signature = crypto.createHmac('sha256', Momo.secretKey).update(rawSignature.toString()).digest('hex');
             console.log(signature)
@@ -74,7 +75,7 @@ class TestController {
                 amount: amountStr,
                 orderId: orderId,
                 orderInfo: orderInfo,
-                redirectUrl: returnUrl,
+                redirectUrl: redirectUrl,
                 ipnUrl: ipnUrl,
                 requestType: "captureWallet",
                 extraData: extraData,
